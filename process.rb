83c5c4724aa22cebdb8a06e90bd080fa4ebc32eb
leo60228 <leo@60228.dev>
2020-05-17 14:26:21 -0400

    cross-origin

--- process.rb
+++ process.rb
@@ -1,7 +1,9 @@
 require 'open-uri'
 require 'nokogiri'
 doc = Nokogiri::XML(open(ARGV[0]))
+bordered_doc = doc.dup
 svg = doc.at_css("svg")
+bordered_svg = bordered_doc.at_css("svg")
 viewBox = "0 0 36 36"
 if svg.key?("viewBox")
     viewBox = svg["viewBox"].split(" ").map { |s| s.to_i }
@@ -14,23 +16,18 @@ if svg.key?("viewBox")
     svg.delete("viewBox")
     viewBox = viewBox.join(" ")
 end
-symbol = Nokogiri::XML::Node.new "symbol", doc
-symbol["viewBox"] = viewBox
-symbol["id"] = "emoji"
-g = Nokogiri::XML::Node.new "g", doc
-for elem in doc.css("svg > *")
+g = Nokogiri::XML::Node.new "g", bordered_doc
+for elem in bordered_doc.css("svg > *")
     border_elem = elem.dup
-    elem.parent = symbol
+
+    if border_elem.key?("fill")
+        border_elem.delete("fill")
+    end
+    border_elem["stroke"] = "white"
+
     style = ""
     if border_elem.key?("style")
         style = border_elem["style"]
-    end
-    border_elem["fill"] = "transparent"
-    if border_elem.key?("stroke")
-        border_elem.delete("stroke")
-        style += " stroke: inherit;"
-    else
-        elem["stroke"] = "transparent"
     end
     old_width = "0px"
     if border_elem.key?("stroke-width")
@@ -38,8 +35,11 @@ for elem in doc.css("svg > *")
     end
     style += " stroke-width: calc(#{old_width} + 4px)"
     border_elem["style"] = style.strip
+
     g.add_child(border_elem)
 end
-symbol.prepend_child(g)
-svg.add_child(symbol)
-print(doc.to_xml)
+bordered_svg.prepend_child(g)
+bordered_svg["viewBox"] = viewBox
+svg["viewBox"] = viewBox
+puts(bordered_doc.to_xml)
+STDERR.puts(doc.to_xml)
